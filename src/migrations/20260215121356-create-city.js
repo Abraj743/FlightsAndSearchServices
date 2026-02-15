2f32eafa6e0e91452c3b6ae54e5bcd7606255a8d
Abhay Raj Singh <abraj.singh743@gmail.com>
2026-02-15 18:04:27 +0530

    Added city model and migration

---
+++ src/migrations/20260215121356-create-city.js
@@ -0,0 +1,30 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    // query interface is the interface using which sequelize or sequelize-cli connects with our database
+    await queryInterface.createTable('Cities', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull:false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Cities');
+  }
+};
